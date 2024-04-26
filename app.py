cc55aa786d49724f0831b839482f2e8ee146122d
talalshafei <talal.sh167@gmail.com>
2024-04-26 18:09:10 +0300

    setup2

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 from flask import Flask, request
+from flask_cors import CORS
+# from dotenv import load_dotenv
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route("/summarizeFile", methods=["POST"])
+def summarizeFile():
+    file = request.files['file']
+    print(file)
+    return "File received"
